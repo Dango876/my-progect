bb950c30fe31f0237afb2269940b41be35f1f697
Artem Vasin <dangelos876@gmail.com>
2025-08-18 17:52:35 +0300

    Hot Fix

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,22 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
+
 public class Main {
+    private final static UserService userService = new UserServiceImpl();
+
     public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+        userService.createUsersTable();
+
+        userService.saveUser("Александр", "Иванов", (byte) 90);
+        userService.saveUser("Мария", "Смирнова", (byte) 80);
+        userService.saveUser("Дмитрий", "Петров", (byte) 70);
+        userService.saveUser("Ольга", "Кузнецова", (byte) 60);
+
+        userService.removeUserById(2);
+        userService.getAllUsers();
+        userService.cleanUsersTable();
+        userService.dropUsersTable();
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,20 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+    public static Connection getConnection() {
+        try {
+            String url = "jdbc:mysql://localhost:3306/world";
+            String username = "root";
+            String password = "root";
+
+            return DriverManager.getConnection(url, username, password);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
 }
